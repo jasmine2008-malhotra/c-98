fedc6934fd2271639b002d03de31c5daf7acd6ad
jasmine2008-malhotra <70573214+jasmine2008-malhotra@users.noreply.github.com>
2022-04-19 20:34:08 +0530

    Add files via upload

---
+++ countingWords.py
@@ -0,0 +1,8 @@
+pocketMoney = int(input("enter your pocket money: "))
+if (pocketMoney > 5000):
+    print("congratulation you are a rich boy ")
+elif (pocketMoney > 1000):
+    print ("good joob you made a mistake and got less money ")
+else : 
+    print ("you are poor")
+    
---
+++ funtion.py
@@ -0,0 +1,14 @@
+def countWordsFromFile():
+    fileName = input("enter the file name: ")
+
+    numberOfWords = 0 
+
+    file = open(fileName,'r')
+    for line in file :
+        words = line.split()
+        numberOfWords = numberOfWords+len(words)
+
+    print("number of words in this file: ")
+    print(numberOfWords)
+
+countWordsFromFile()
